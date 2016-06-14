690d4463b23ca19da89d6a9a483bc4962ed3d10a
Say <sktjdgns1189@naver.com>
2016-06-15 04:11:32 +0900

    update dtbTool

--- mkdt.sh
+++ mkdt.sh
@@ -1,4 +1,4 @@
 rm ./zImage
 rm ./dt.img
-./dtbTool -o ./dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
+./dtbTool -2 -o ./dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
 cp ./arch/arm/boot/zImage ./
