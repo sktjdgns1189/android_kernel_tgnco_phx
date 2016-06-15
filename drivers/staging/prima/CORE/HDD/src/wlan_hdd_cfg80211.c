e64151e72397ab2f56f564ebdf3b21ea5df01a8c
Say <sktjdgns1189@naver.com>
2016-06-15 13:58:47 +0900

    prima: Update driver to LA.BF.1.1.3_rb1.12

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -11026,7 +11026,8 @@ int __wlan_hdd_cfg80211_scan( struct wiphy *wiphy,
        else
            hddLog(VOS_TRACE_LEVEL_ERROR, "%s: TDLS teardown is ongoing %d",
                                           __func__, status);
-       goto free_mem;
+
+       return status;
     }
 #endif
 
